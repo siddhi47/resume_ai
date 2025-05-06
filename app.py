0d371bd4683cd8dc32168e77c349a09981283998
Siddhi Bajracharya <siddhi@i8labs>
2025-05-06 14:02:31 -0700

    updated creds

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
 from src.resume_bot.auth import users, User
 from src.resume_bot.forms import LoginForm, ResumeChatForm
+import hashlib
 
 # LangChain & OpenAI
 from langchain.chat_models import ChatOpenAI
@@ -41,7 +42,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
-        if username in users and users[username]["password"] == form.password.data:
+        password = form.password.data
+        password = hashlib.md5(password.encode()).hexdigest()
+        if username in users and users[username]["password"] == password:
             login_user(User(username))
             return redirect(url_for("chat"))
         else:
@@ -62,6 +65,7 @@ def chat():
 
     if form.validate_on_submit():
         job_desc = form.job_description.data
+        # summarize job description
         query = form.question.data
 
         if form.resume.data:
@@ -104,13 +108,31 @@ def chat():
                 resume_summary = session["resume_summary"]
 
         llm = ChatOpenAI(temperature=0.5)
+        # Summarize job description prompt
 
+        summary_prompt = PromptTemplate(
+            input_variables=["job_description"],
+            template="""
+                You are an expert in job descriptions. Summarize the following job description into:
+                0. Location
+                1. Job Title
+                2. Key Responsibilities
+                3. required Skills
+                4. Preferred Qualifications
+                5. Any other relevant information
+                Job Description:
+                {job_description}
+                """,
+        )
+        summary_chain = LLMChain(llm=llm, prompt=summary_prompt)
+        job_summary = summary_chain.run(job_description=job_desc)
         if "cover letter" in query.lower():
             cover_prompt = PromptTemplate(
                 input_variables=["resume_summary", "job_description"],
                 template="""
                 Based on the following resume and job description, write a professional cover letter tailored for this position.
-
+                You are to use the relevant information from the resume and job description to create a personalized cover letter.
+                Talk about willingness to relocate if the job is far away from the location in resume.
                 Resume:
                 {resume_summary}
 
@@ -119,7 +141,9 @@ def chat():
                 """,
             )
             chain = LLMChain(llm=llm, prompt=cover_prompt)
-            answer = chain.run(resume_summary=resume_summary, job_description=job_desc)
+            answer = chain.run(
+                resume_summary=resume_summary, job_description=job_summary
+            )
 
         else:
             generic_prompt = PromptTemplate(
@@ -142,7 +166,7 @@ def chat():
             )
             prompt = generic_prompt.format(
                 resume_summary=resume_summary,
-                job_description=job_desc,
+                job_description=job_summary,
                 user_question=query,
             )
             answer = llm.predict(prompt)
--- src/resume_bot/auth.py
+++ src/resume_bot/auth.py
@@ -1,6 +1,6 @@
 from flask_login import UserMixin
 
-users = {"siddhi": {"password": ""}}
+users = {"siddhi": {"password": "482c811da5d5b4bc6d497ffa98491e38"}}
 
 
 class User(UserMixin):
